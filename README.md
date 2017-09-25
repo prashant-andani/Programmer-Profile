da0cb465980f5ac441e8bc51e2409b79d7136fa9
Guruprasad <ghonnesh@innominds.com>
2017-09-25 14:52:03 +0530

    Latest Changes

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# ProgProfile
+# Programmer Profile
+This app lets a programmer create a profile of all online presence in to a single page. 
+i.e a Github, Stackoverflow, LindkedIn Etc.
+
+This can be a potential online Resume of a developer where he/she can showcase his online presence and invlovement as a programmer.
+Note: This app is not hardcoded to show up the profile details of mine..
+
+Plase change in the config to put the urls of yours
+
+How to Run a app?
+
+Download or clone 
+Get in to the project directory
+npm install
+
+then 
+ng serve - to run the project
+
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.4.3.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
+import { GitHubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
+  githubData: Object;
+  soData: Object;
+  reposUrl: string;
+  repoList: Object;
+  constructor(private Service: DataService, private GitHubService: GitHubService) {
+    //this.jsFiddle();
+  }
+
+  jsFiddle() {
+    this.Service.getJsFiddles().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
